a2cf7f7a7cd258ddbfc95a66b96538cb9368212c
Lucas Bento <lucas.bento@b2wdigital.com>
2016-12-13 11:57:13 -0200

    Altera os limites de sessões processos e transações

--- assets/setup.sh
+++ assets/setup.sh
@@ -41,6 +41,29 @@ echo 'export ORACLE_SID=XE' >> /etc/bash.bashrc &&
 mv /assets/startup.sh /usr/sbin/startup.sh &&
 chmod +x /usr/sbin/startup.sh &&
 
+# Alter sessions max #################################
+LISTENER_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
+TNSNAMES_ORA=/u01/app/oracle/product/11.2.0/xe/network/admin/tnsnames.ora
+
+cp "${LISTENER_ORA}.tmpl" "$LISTENER_ORA" &&
+sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENER_ORA}" &&
+sed -i "s/%port%/1521/g" "${LISTENER_ORA}" &&
+cp "${TNSNAMES_ORA}.tmpl" "$TNSNAMES_ORA" &&
+sed -i "s/%hostname%/$HOSTNAME/g" "${TNSNAMES_ORA}" &&
+sed -i "s/%port%/1521/g" "${TNSNAMES_ORA}" &&
+
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+export PATH=$ORACLE_HOME/bin:$PATH
+export ORACLE_SID=XE
+
+echo "alter system set processes = 150 scope = spfile;" | sqlplus -s SYSTEM/oracle
+echo "alter system set sessions = 300 scope = spfile;" | sqlplus -s SYSTEM/oracle
+echo "alter system set transactions = 330 scope = spfile;" | sqlplus -s SYSTEM/oracle
+
+service oracle-xe restart
+
+#########################################################
+
 # Remove installation files
 rm -r /assets/
 
